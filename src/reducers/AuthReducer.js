7ac813bd495731b6cf3a52c72005197b3fa594c3
Ibrahim Al-Khalifah <iheemoo@gmail.com>
2018-01-02 11:34:59 +0300

    Fixed undefined state for the moment in the Auth Reducer

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,9 +1,11 @@
 import { MOBILE_CHANGED } from '../actions/types';
 
 const INITIAL_STATE = { mobile: '' };
-
+//NOTE: remember dont return undefined state
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
+        case MOBILE_CHANGED:
+            return state;
         default:
             return state;
     }
